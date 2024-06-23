d8c58a86aed0e0a5e158fd2e0b2ff4efa5173ac0
Ivan Markovic <ivan.si400m@icloud.com>
2024-06-23 22:28:08 +0200

    added spinner

--- src/app/page.client.js
+++ src/app/page.client.js
@@ -4,21 +4,28 @@ import DetailsColorModal from "@/components/DetailsColorModal";
 import ColorFilter from "@/components/ColorFilter";
 import ColorList from "@/components/ColorList";
 import ColorForm from "@/components/ColorForm";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useStore} from "@/state/store";
+import Spinner from "@/components/Spinner";
 
 export default function HomeClient() {
     const fetchColors = useStore(state => state.fetchColors);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetchColors();
+        fetchColors()
+            .then(() => setLoading(false))
+            .catch(error => {
+                console.error('Error fetching colors:', error);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <div className="container mx-auto p-4 text-black">
             <h1 className="text-2xl font-bold mb-4">Color Manager</h1>
             <ColorFilter/>
-            <ColorList/>
+            {loading ? <Spinner/> : <ColorList/>}
             <ColorForm/>
             <DetailsColorModal/>
         </div>
---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spinner = () => (
+    <div className="flex justify-center items-center mt-4">
+        <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-b-green-600"></div>
+    </div>
+);
+
+export default Spinner;
